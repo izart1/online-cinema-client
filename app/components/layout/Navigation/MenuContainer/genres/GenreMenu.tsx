82b0c00ca5dc8a53d0bd046ceb9086b791511752
Artem Izmailov <temalinho@gmail.com>
2023-05-16 00:06:21 +0300

    add Seo

--- app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
+++ app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import SkeletonLoader from '@/components/ui/SkeletonLoader';
+
 import Menu from '../Menu';
 
 import { usePopularGenres } from './usePopularGenres';
@@ -7,7 +9,9 @@ import { usePopularGenres } from './usePopularGenres';
 const GenreMenu: FC = () => {
   const { isLoading, data } = usePopularGenres();
   return isLoading ? (
-    <div className="mx-11 mb-6">...Loading</div>
+    <div className="mx-11 mb-6">
+      <SkeletonLoader count={5} className="h-7 mt-6" />
+    </div>
   ) : (
     <Menu menu={{ title: 'Popular genres', items: data || [] }} />
   );
