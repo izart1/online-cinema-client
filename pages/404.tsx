421cf99a43063eea20d7fbbd8529a8cefb144be6
Artem Izmailov <temalinho@gmail.com>
2023-05-16 13:17:23 +0300

    add Popular Movies, 404 and 500 Error page

---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Heading from '@/components/ui/heading/Heading';
+
+import Meta from '@/utils/meta/Meta';
+
+const Error404 = () => {
+  return (
+    <Meta>
+      <Heading title="404 Page Not Found" />
+    </Meta>
+  );
+};
+
+export default Error404;
---
+++ pages/500.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Heading from '@/components/ui/heading/Heading';
+
+import Meta from '@/utils/meta/Meta';
+
+const Error500 = () => {
+  return (
+    <Meta>
+      <Heading title="500 Internal Server Error" />
+    </Meta>
+  );
+};
+
+export default Error404;
